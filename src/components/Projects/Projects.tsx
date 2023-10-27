d774ba60e5737380f16a7dc3d5fef2a39ad5c7db
Franklin Huichi <carloshuichi@gmail.com>
2023-10-27 10:22:18 -0500

    Add components

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import TitleSection from "../TitleSection/TitleSection";
 import projects from "../../information/portfolio.json";
+import components from "../../information/components.json";
 import CardProject from "../CardProject/CardProject";
 import "./Projects.css";
+import ComponentProject from "../ComponentProject/ComponentProject";
+
 const Projects = () => {
   return (
     <>
@@ -19,6 +22,13 @@ const Projects = () => {
               .map((project, index) => (
                 <CardProject key={project.id} index={index} {...project} />
               ))}
+          </div>
+          <div className="projects__cards">
+            <aside className="project__components">
+              {components.map((props) => (
+                <ComponentProject key={props.id} {...props} />
+              ))}
+            </aside>
           </div>
         </div>
       </section>
